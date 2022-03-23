5308dac70e0fddff410c0e9dd0dc4cb965ba519f
napo <napo@fbk.eu>
2022-03-23 12:18:26 +0100

    update

--- bin/download_csv_seminars.sh
+++ bin/download_csv_seminars.sh
@@ -1,1 +1,2 @@
 wget "https://docs.google.com/spreadsheets/d/e/2PACX-1vT-1hbZ9phjvuPZ69h-dVa6gYtGjvAUr1QFxCgmx9CSvug49eFgHzOwLvQBQEJdbSYREmU1UlNOQHoq/pub?gid=137863374&single=true&output=csv" -O seminars.csv
+cp seminars.csv ../docs/_data/
